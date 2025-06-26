7ff996a5ae268ed2ee1915ab9e110eb976bd43c7
ziadAhmed27 <ziadahmad2793@gmail.com>
2025-06-26 16:42:03 +0300

    Add root route for friendly API message

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ app.post('/signin', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.send('Customer API is running!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
